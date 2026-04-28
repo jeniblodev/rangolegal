1101b8a28d6e0e2b4bc2e9956f4cdf0c9157b952
jeniblodev <lo.jbittencourt@gmail.com>
2026-04-27 21:13:52 -0300

    Initial commit

---
+++ src/main/java/com/food/rangolegal/RangolegalApplication.java
@@ -0,0 +1,13 @@
+package com.food.rangolegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangolegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangolegalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/rangolegal/RangolegalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.rangolegal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangolegalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
