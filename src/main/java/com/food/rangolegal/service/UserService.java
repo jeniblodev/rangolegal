592e065d6ec2dc922fe9b040bf2a61594e4779b4
thiago-enjo <thiago.yuji@hotmail.com>
2026-05-05 19:48:38 -0300

    Corrige import do Transactional.

--- src/main/java/com/food/rangolegal/service/UserService.java
+++ src/main/java/com/food/rangolegal/service/UserService.java
@@ -10,6 +10,7 @@ import com.food.rangolegal.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
